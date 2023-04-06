609d93c2beb9a5f1c5b2f27b6ec0c9cdb7a218bd
TuanHungPham <rrvqth@gmail.com>
2023-04-07 00:28:49 +0700

    Commit at 7/4/2023

---
+++ Assets/Scripts/Item/ItemSpawnerSystem/ItemSpawner.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSpanwer : Spawner
+{
+    #region public var
+    public bool CanDrop { get => canDrop; set => canDrop = value; }
+    #endregion
+
+    #region private var
+    [SerializeField] protected bool canDrop;
+
+    #endregion
+
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    protected override void Reset()
+    {
+        base.Reset();
+    }
+
+    protected override void LoadComponents()
+    {
+        parent = transform;
+    }
+
+    private void Update()
+    {
+        Spawn();
+        UpdateListGameObj();
+        ClearActiveList();
+    }
+
+    public override void Spawn()
+    {
+        base.Spawn();
+    }
+
+    public virtual void GetSpawnPos(Transform position)
+    {
+        spawnPos = position;
+    }
+
+    protected override void GetObjFromList()
+    {
+        base.GetObjFromList();
+    }
+
+    protected override GameObject RandomGameObj()
+    {
+        return base.RandomGameObj();
+    }
+
+    protected override GameObject NewGameObj(GameObject obj)
+    {
+        return base.NewGameObj(obj);
+    }
+
+    protected override void SetActiveObj()
+    {
+        base.SetActiveObj();
+    }
+
+    protected override void UpdateListGameObj()
+    {
+        foreach (Transform child in transform)
+        {
+            if (!child.gameObject.activeSelf && !listOfInactiveObj.Contains(child.gameObject))
+            {
+                listOfInactiveObj.Add(child.gameObject);
+            }
+        }
+    }
+
+    protected virtual void ClearActiveList()
+    {
+        if (!EnemyWaveManager.Instance.UpdateWave) return;
+
+        listOfActiveObj.Clear();
+        maxObj = 0;
+    }
+
+    protected override bool CanSpawn()
+    {
+        if (canDrop && listOfActiveObj.Count < maxObj) return true;
+
+        return false;
+    }
+}
