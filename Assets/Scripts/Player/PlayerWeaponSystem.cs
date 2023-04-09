62fb469fd78df39b8d945abae0c7fcbce7b3ff5b
TuanHungPham <rrvqth@gmail.com>
2023-04-09 16:29:04 +0700

    9/4/2023 4:28PM

--- Assets/Scripts/Player/PlayerWeaponSystem.cs
+++ Assets/Scripts/Player/PlayerWeaponSystem.cs
@@ -23,13 +23,8 @@ public class PlayerWeaponSystem : MonoBehaviour
 
     #region private var
     [SerializeField] private PlayerCtrl playerCtrl;
-    [SerializeField] private WeaponInventoryPanel weaponInventoryPanel;
 
     [Space]
-    [SerializeField] private Transform weaponStorage;
-    [SerializeField] private Transform primaryWeaponHolder;
-    [SerializeField] private Transform secondaryWeaponHolder;
-    [SerializeField] private Transform meleeWeaponHolder;
     [SerializeField] private Transform crosshair;
 
     [Space]
@@ -54,19 +49,13 @@ public class PlayerWeaponSystem : MonoBehaviour
 
         playerCtrl = GetComponentInParent<PlayerCtrl>();
 
-        weaponInventoryPanel = GameObject.Find("------ UI ------").transform.GetChild(0).GetComponentInChildren<WeaponInventoryPanel>();
-        weaponStorage = GameObject.Find("------ ITEM ------").transform.Find("WeaponStorage");
         crosshair = GameObject.Find("------ PLAYER ------").transform.Find("AimingSystem").GetChild(0);
-        primaryWeaponHolder = transform.Find("PrimaryWeapon").Find("Holder");
-        secondaryWeaponHolder = transform.Find("SecondaryWeapon").Find("Holder");
 
         enemyLayer = LayerMask.GetMask("Enemy");
     }
 
     private void Update()
     {
-        GetWeaponFromStorage();
-        SwitchWeapon();
         GetWeaponInHolder();
 
         FlipWeapon();
@@ -127,79 +116,6 @@ public class PlayerWeaponSystem : MonoBehaviour
         else
         {
             selectedWeapon.rotation = lastRotation;
-        }
-    }
-
-    private void GetWeaponFromStorage()
-    {
-        if (!playerCtrl.playerWeaponInventory.IsUpdateInventory) return;
-
-        for (int i = 0; i < playerCtrl.playerWeaponInventory.weaponInventory.Count; i++)
-        {
-            Transform weapon = GetWeaponInStorageByName(playerCtrl.playerWeaponInventory.weaponInventory[i]);
-
-            if (weapon == null) continue;
-
-            PutWeaponToHolder(weapon);
-            weapon.localPosition = Vector3.zero;
-            weapon.gameObject.SetActive(true);
-        }
-
-        playerCtrl.playerWeaponInventory.IsUpdateInventory = false;
-    }
-
-    private void PutWeaponToHolder(Transform obj)
-    {
-        Weapon weapon = obj.GetComponent<Weapon>();
-
-        if (weapon.weaponData.WeaponType == WeaponType.PRIMARY_WEAPON)
-        {
-            obj.SetParent(primaryWeaponHolder);
-        }
-        else if (weapon.weaponData.WeaponType == WeaponType.SECONDARY_WEAPON)
-        {
-            obj.SetParent(secondaryWeaponHolder);
-        }
-    }
-
-    private Transform GetWeaponInStorageByName(WeaponData weaponData)
-    {
-        foreach (Transform item in weaponStorage)
-        {
-            Weapon weapon = item.GetComponent<Weapon>();
-
-            if (!weapon.weaponData.WeaponName.Equals(weaponData.WeaponName)) continue;
-
-            return item;
-        }
-        return null;
-    }
-
-    private void SwitchWeapon()
-    {
-        if (!weaponInventoryPanel.IsWeaponSwitched) return;
-
-        foreach (WeaponHolderUI weaponHolderUI in weaponInventoryPanel.listOfWeaponHolderUI)
-        {
-            if (!weaponHolderUI.IsSelected) continue;
-
-            GetHolder(weaponHolderUI);
-            return;
-        }
-    }
-
-    private void GetHolder(WeaponHolderUI weaponHolderUI)
-    {
-        primaryWeaponHolder.gameObject.SetActive(false);
-        secondaryWeaponHolder.gameObject.SetActive(false);
-
-        if (weaponHolderUI.holderType == HolderType.PRIMARY_HOLDER)
-        {
-            primaryWeaponHolder.gameObject.SetActive(true);
-        }
-        else if (weaponHolderUI.holderType == HolderType.SECONDARY_HOLDER)
-        {
-            secondaryWeaponHolder.gameObject.SetActive(true);
         }
     }
 
